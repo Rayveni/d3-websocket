14ac893cd88b6b072ff8162df3d1f688b87cd860
I.V <is.volochkov@gmail.com>
2023-07-13 23:48:56 +0300

    save2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 var connection = new WebSocket('ws://signal.mmr.systems:8080');
 var hist_data = [[]];
-init_chart('#chart-container');
+var time_range=null, value_range=null;
+//init_chart('#chart-container');
+  // Calculate Margins and canvas dimensions
+  const margin = { top: 40, right: 40, bottom: 40, left: 40 },
+    width = 0.9 * window.innerWidth - margin.left - margin.right,
+  height = 0.9 * window.innerHeight - margin.top - margin.bottom;
+const tick_num = { left: null, right: null, bottom: null };
 connection.onmessage = function (event) {
   var new_event = JSON.parse(event.data);
   update_data(new_event);
@@ -17,7 +23,17 @@ connection.onmessage = function (event) {
 function update_data(new_event) {
   const event_time = new_event.time,
     values = new_event.values;
-
+  
+  let event_value_range = [Math.min.apply(Math, values),Math.max.apply(Math, values)];
+  if (time_range == null) {
+    time_range = [event_time, event_time];
+    value_range = event_value_range;
+  } else
+  {
+    time_range = [time_range[0], event_time];
+    value_range = [Math.min(value_range[0],event_value_range[0]),Math.max(value_range[1],event_value_range[1])];
+    }
+    
   for (let i = 0; i < values.length - hist_data.length; i++) {
     hist_data.push([]);
   }
